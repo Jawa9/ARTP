d61f77daee89cea9c920426e4e9a86ac7d0eaec8
Neko <baxbani1990@gmail.com>
2017-10-08 22:10:43 +0300

    Прога которая помогает решать проблемы с кодом работая в команде

---
+++ app/src/main/java/com/learn/neko/artp/ActivityDoNotTouch.java
@@ -0,0 +1,13 @@
+package com.learn.neko.artp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDoNotTouch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_do_not_touch);
+    }
+}
